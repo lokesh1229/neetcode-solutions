cc050d74f26a3192bab444496c6eacaef0b4067c
PONNA LOKESH KUMAR <116939110+lokesh1229@users.noreply.github.com>
2026-04-21 23:03:37 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = Integer.MIN_VALUE;
+        int minPrice = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            minPrice = Math.min(minPrice,prices[i]);
+            maxProfit = Math.max(maxProfit,prices[i]-minPrice);
+        }
+        return maxProfit;
+
+    }
+}
